a809d909626bcc92f0fd5ddc400243018584b0c3
Karan Bhamra <karan.bhamra@gmail.com>
2020-06-27 15:16:50 -0700

    Add project files.

---
+++ GithubActionsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubActionsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
